f92195893aa7cc0fb09d6e598e29a420cafdb0b8
qiyu <qiyu@souche.com>
2020-10-13 15:33:35 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
 
-with open('requirements.txt') as f:
-    install_requires = list(filter(None, (f.read()).splitlines()))
+install_requires = list(filter(None, ('''
+
+xmltodict==0.12.0
+requests==2.24.0
+pydash==4.8.0
+
+''').splitlines()))
+
 setup(
     name='alfred_youdao_dict',
     version='1.0.0',
@@ -11,7 +17,9 @@ setup(
     url='https://github.com/qiyu/alfred-youdao-dict',
     maintainer='qiyu',
     package_data={
+        '': ['*.txt']
     },
+    include_package_data=True,
     packages=['alfred_youdao_dict'],
     install_requires=install_requires
 )
